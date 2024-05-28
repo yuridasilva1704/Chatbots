d5766c64077c9b13962016765ef71c393d10b021
yuridasilva1704 <51483291+yuridasilva1704@users.noreply.github.com>
2024-05-28 20:09:52 -0300

    Add files via upload

---
+++ CardapioNilson/src/menu.js
@@ -0,0 +1,28 @@
+// menu.js
+export const menu = {
+  1: {
+    description: 'Feijão Preto',
+    // price: 6,
+    available: true
+  },
+  2: {
+    description: 'Arroz',
+    // price: 6,
+    available: true
+  },
+  3: {
+    description: 'Macarrão',
+    // price: 6,
+    available: true
+  },
+  4: {
+    description: 'Porco',
+    // price: 6,
+    available: true
+  },
+  5: {
+    description: 'Bisteca',
+    // price: 6,
+    available: true
+  },
+};
