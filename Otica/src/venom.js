ce0b47fb960ffbaa8565dced5dc73736d9997696
Yuri Silva <yuri.silva.elite@gmail.com>
2024-06-28 13:43:21 -0300

    Codigo atualizado

--- Otica/src/venom.js
+++
@@ -1,9 +0,0 @@
-const venom = require('venom-bot');
-
-class VenomBotSingleton {
-  static async createInstance(sessionName) {
-    return venom.create(sessionName);
-  }
-}
-
-module.exports = { VenomBotSingleton };
